eea7795b3db0c1ad8d11bc783642fcd3d22e274f
aditya-mkhy <mahadevadityamukhiya@gmail.com>
2025-11-22 17:37:36 -0800

    + log

--- esp32/util.py
+++ esp32/util.py
@@ -202,6 +202,63 @@ def update_time():
     except Exception as e:
         print("Failed to update time:", e)
         return False
- 
+    
+class Logger:
+    def __init__(self, flush_limit=2):
+        self.log_dir = "./log"
+        self.buffer = []
+        self.flush_limit = flush_limit
+        self.max_file_size =  10 #1048576 * 5 # 5 MB
+        self.last_num = self._get_num_of_last_file()
+        self.path = f"{self.log_dir}/log-{self.last_num}.txt"
+        self.update_path()
+
+    def _get_num_of_last_file(self):
+        last_num = 1
+        try:
+            for file in os.listdir(self.log_dir):
+                num_str = file[file.rfind("-") + 1 : file.rfind(".")]
+                try:
+                    num = int(num_str)
+                except:
+                    num = 1
+                last_num = max(last_num, num)
+        except:
+            pass
+        return last_num
+        
+
+    def update_path(self):
+        try:
+            size = os.stat(self.path)[6]
+        except:
+            size = 0
+
+        if size  < self.max_file_size:
+            return
+        
+        self.last_num += 1
+        self.path = f"{self.log_dir}/log-{self.last_num}.txt"    
+            
+
+    def log(self, msg: str):
+        self.buffer.append(msg + "\n")
+        if len(self.buffer) >= self.flush_limit:
+            self.flush()
+
+    def flush(self):
+        self.update_path()
+        try:
+            with open(self.path, "a") as f:
+                for line in self.buffer:
+                    f.write(line)
+            self.buffer = []
+        except Exception as e:
+            print("[LOG-ERROR]", e)
+
+
 if __name__ == "__main__":
-    print(filename("/sd/name/pala/k.txt"))
+    logger = Logger()
+    print(logger.path)
+    log = logger.log
+    print(logger.path)
